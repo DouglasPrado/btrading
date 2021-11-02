11838f9695c9b4c076fe548b3d67bc70441fc0f6
Douglas Prado <douglasrafaeldoprado@gmail.com>
2021-11-02 13:03:51 -0300

    Initial commit

---
+++ src/components/Common/index.ts
@@ -0,0 +1,6 @@
+import Button from './Button'
+import Footer from './Footer'
+import Header from './Header'
+import ThemeSwitcher from './ThemeSwitcher'
+
+export { Button, Footer, Header, ThemeSwitcher }
